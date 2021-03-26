5fb51212b0ac2ad71176ba05a658fb58c2b1d224
nishant <nishant.chgh@gmail.com>
2021-03-26 14:50:53 +0530

    added hamburger icon for small devices and necessary JS for it

---
+++ script.js
@@ -0,0 +1,21 @@
+// to show nav links when hamburger icon is clicked
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav_list");
+
+hamburger.addEventListener("click" ,open)
+
+function open (e) {
+    console.log("hello")
+    navList.classList.toggle("open")
+}
+
+// to remove open class when an nav link is clicked
+const menu = document.querySelectorAll(".nav_list__item")
+
+menu.forEach((link)=>{
+    link.addEventListener("click", removeOpen)
+
+    function removeOpen(e){
+        navList.classList.remove("open")
+    }
+})
